78e3a826b9482038768683f2170da95e32435d0e
mba favour <mbahf64@gmail.com>
2024-07-22 02:39:59 +0100

    second-commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,13 @@
-
-
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "",
-  description: "",
-};
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
-      <body className=" pt-[5px] xl:pt-5">
-  
-        {children}</body>
+      <body>{children}</body>
     </html>
-  );
+  )
 }
